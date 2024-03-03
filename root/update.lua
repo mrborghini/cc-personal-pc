30f48a4c2aa1b4954ddf61d131a5a9f70aef1046
lamborghinigamer1 <61702082+lamborghinigamer1@users.noreply.github.com>
2024-03-03 20:29:42 +0100

    Fixed update tool being too fast

--- root/update.lua
+++ root/update.lua
@@ -13,8 +13,8 @@ function DeleteCurrent()
 end
 
 function ApplyUpdate()
-    shell.openTab("wget https://raw.githubusercontent.com/lamborghinigamer1/cc-personal-pc/master/setup.lua")
-    shell.openTab("/setup")
+    shell.open("wget https://raw.githubusercontent.com/lamborghinigamer1/cc-personal-pc/master/setup.lua")
+    shell.run("/setup")
 end
 
 function Main()
