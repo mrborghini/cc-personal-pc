f3e45465cdc70c4aba2b0c86205142f6c3725c1f
lamborghinigamer1 <61702082+lamborghinigamer1@users.noreply.github.com>
2024-03-03 20:38:14 +0100

    Removed -q flag

--- root/utils/update-update-tool.lua
+++ root/utils/update-update-tool.lua
@@ -1,6 +1,6 @@
 function Main()
     shell.run("rm /update.lua")
-    shell.run("wget -q https://raw.githubusercontent.com/lamborghinigamer1/cc-personal-pc/master/root/update.lua")
+    shell.run("wget https://raw.githubusercontent.com/lamborghinigamer1/cc-personal-pc/master/root/update.lua")
 end
 
 Main()
