510a2f5c19a57aa6a151f045b1d08d0422059a5a
mrborghini <61702082+mrborghini@users.noreply.github.com>
2025-05-31 17:04:52 +0200

    feat: add ascii art for different screen sizes

--- root/utils/ascii.lua
+++ root/utils/ascii.lua
@@ -1,3 +1,10 @@
+local function show_art(art)
+    for i = 1, #art, 1 do
+        print(art[i])
+        sleep(0.0005)
+    end
+end
+
 function Main()
     local art = {
         "            _                  _    _      _ ",
@@ -6,9 +13,30 @@ function Main()
         " |_|_|_|_| |_.__/\\___/_| \\__, |_||_|_|_||_|_|",
         "                         |___/               "
     }
-    for i = 1, #art, 1 do
-        print(art[i])
-        sleep(0.0005)
+
+    local art_small = {
+        "  _               _         ",
+        " | |   __ _ _ __ | |__  ___ ",
+        " | |__/ _` | '  \\| '_ \\/ _ \\",
+        " |____\\__,_|_|_|_|_.__/\\___/"
+    }
+
+    local art_smallest = {
+        "    _           ",
+        "   (_)__ _ _ _  ",
+        "   | / _` | ' \\ ",
+        "  _/ \\__,_|_||_|",
+        " |__/           "
+    }
+
+    local width, _ = term.getSize()
+
+    if width >= 45 then
+        show_art(art)
+    elseif width >= 28 then
+        show_art(art_small)
+    else
+        show_art(art_smallest)
     end
 end
 
